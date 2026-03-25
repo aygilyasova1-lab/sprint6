f508a4d91a31f44af410422bc1c068820856233b
Ilyasova Aygel <aygilyasova1@gmail.com>
2026-03-25 15:43:01 +0300

    7

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -7,6 +7,7 @@ import (
 	"path/filepath"
 	"time"
 	service "github.com/Yandex-Practicum/go1fl-sprint6-final/internal/service"
+	"strings"
 )
 
 func RootHandler(w http.ResponseWriter, r *http.Request) {
@@ -23,49 +24,59 @@ func RootHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func UploadHandler(w http.ResponseWriter, r *http.Request) {
-	file, header, err := r.FormFile("upload")
 	var data []byte
-	if err == nil {
-		defer file.Close()
-		data, err = io.ReadAll(file)
+	var fileName string
+	contentType := r.Header.Get("Content-Type")
+
+	if strings.Contains(contentType, "multipart/form-data") {
+
+		file, header, err := r.FormFile("upload")
+ 		if err != nil {
+			data, err = io.ReadAll(r.Body)
+   			if err != nil {
+   				http.Error(w, "ошибка при чтении данных", http.StatusInternalServerError)
+   				return
+  				}
+			fileName = "body_" + time.Now().UTC().Format("20060102_150405") + ".txt"
+  		} else {
+   		defer file.Close()
+  		data, err = io.ReadAll(file)
 		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-		    w.Write([]byte("ошибка  при чтении файла"))
+			http.Error(w, "ошибка при чтении файла", http.StatusInternalServerError)
+			return
+ 			}
+   		fileName = time.Now().UTC().Format("20060102_150405") + filepath.Ext(header.Filename)
 		}
-		} else {
+	} else {
+		var err error
 		data, err = io.ReadAll(r.Body)
 		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte("ошибка при чтении запроса"))
-			} 
-		}
-	message := string(data)
-	result, err := service.TextDetector(message)
-	if err != nil {
-		result = message
-	}
+			http.Error(w, "ошибка при чтении тела запроса", http.StatusInternalServerError)
+			return
+ 			}
+ 		 fileName = "body_" + time.Now().UTC().Format("20060102_150405") + ".txt"
+ 		}
 
-	fileName := time.Now().UTC().Format("20060102_150405") + filepath.Ext(header.Filename)
+ 	message := string(data)
+
+	result, err := service.TextDetector(message)
+ 	if err != nil {
+		result = message 
+ 	}
 
 	localFile, err := os.Create(fileName)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("ошибка создания файла"))
-		return
-	}
-	w.Header(). Set("Content-Type", "text/plain")
-	w.Write([]byte(result))
+		http.Error(w, "ошибка создания файла", http.StatusInternalServerError)
+  		return
+		}
 
-	defer localFile.Close()
-
+ 	defer localFile.Close()
 	_, err = localFile.Write([]byte(result))
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("ошибка при записи данных"))
+		http.Error(w, "ошибка при записи данных", http.StatusInternalServerError)
 		return
-	} 
-	
-	w.Header().Set("Content-Type", "text/plain")
-	w.Write([]byte(result))
+		}
 
+	w.Header().Set("Content-Type", "text/plain")
+ 	w.Write([]byte(result))
 }
